d671a5ef5a4254f9639e7167b5e9e14cf59b49e0
Carl Johnson IV <carl@protectamerica.com>
2020-01-03 23:21:21 +0000

    added tests for @require

--- tests/decorators.py
+++ tests/decorators.py
@@ -1,10 +1,10 @@
 from functools import wraps
 from inspect import signature
-from types import SimpleNamespace
 
+from invoke import MockContext
 import pytest
 
-import surge.decorators as tarmod
+import surge.decorators as tarmod #target module
 
 
 class dtask:
@@ -18,14 +18,80 @@ class the_works:
     pass
 
 class require:
-    pass
+    def good(self):
+        return_value = object()
+        decorated_function = tarmod.require('blah', 'blarg')(lambda c: return_value)
+        context = MockContext()
+        context.deploy = {'blah': 'blarg'}
+        
+        result = decorated_function(context)
+        
+        assert result is return_value
+    
+    def bad_error(self):
+        return_value = object()
+        decorated_function = tarmod.require('blah', 'blarg', error=True)(lambda c: return_value)
+        context = MockContext()
+        context.deploy = {'blah': 'blam'}
+        
+        with pytest.raises(tarmod.MissedRequirement):
+            decorated_function(context)
+    
+    def bad_skip(self):
+        return_value = object()
+        decorated_function = tarmod.require('blah', 'blarg', error=False)(lambda c: return_value)
+        context = MockContext()
+        context.deploy = {'blah': 'blam'}
+        context.called_task = 'something'
+        
+        result = decorated_function(context)
+        
+        assert result is None
+    
+    def bad_skip_no_tag(self):
+        return_value = object()
+        decorated_function = tarmod.require('blah', 'blarg', error=False)(lambda c: return_value)
+        context = MockContext()
+        context.deploy = {'blah': 'blam'}
+        
+        with pytest.raises(AttributeError, match='called_task'):
+            decorated_function(context)
+    
+    def missing_error(self):
+        return_value = object()
+        decorated_function = tarmod.require('blah', 'blarg', error=True)(lambda c: return_value)
+        context = MockContext()
+        context.deploy = {'blam': 'blarge'}
+        
+        with pytest.raises(tarmod.MissedRequirement):
+            decorated_function(context)
+    
+    def missing_skip(self):
+        return_value = object()
+        decorated_function = tarmod.require('blah', 'blarg', error=False)(lambda c: return_value)
+        context = MockContext()
+        context.deploy = {'blam': 'blarg'}
+        context.called_task = 'something'
+        
+        result = decorated_function(context)
+        
+        assert result is None
+    
+    def missing_skip_no_tag(self):
+        return_value = object()
+        decorated_function = tarmod.require('blah', 'blarg', error=False)(lambda c: return_value)
+        context = MockContext()
+        context.deploy = {'blam': 'blarg'}
+        
+        with pytest.raises(AttributeError, match='called_task'):
+            decorated_function(context)
 
 class tag_original:
     def plain(self):
         def original_function(c, a, b=None):
             return c
         
-        fake_context = SimpleNamespace()
+        fake_context = MockContext()
         decorated_function = tarmod.tag_original(original_function)
         
         result = decorated_function(fake_context, 4, b=77)
@@ -41,14 +107,14 @@ class tag_original:
         
         def easy_wrap(f):
             @wraps(f)
-            def inner(*a, **kw):
+            def wrapper(*a, **kw):
                 return f(*a, **kw)
             
-            return inner
+            return wrapper
         
         wrapped_function = easy_wrap(original_function)
         
-        fake_context = SimpleNamespace()
+        fake_context = MockContext()
         decorated_function = tarmod.tag_original(wrapped_function)
         
         result = decorated_function(fake_context, 4, b=77)
